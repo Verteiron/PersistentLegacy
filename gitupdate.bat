5ed046ea2974227a4ab16ddc1d0f3368970f4ec8
Verteiron <verteiron@gmail.com>
2016-01-29 20:53:42 -0600

    Collect updates from dep, too.

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,5 +1,8 @@
 @echo off
 SET SOURCEDIR=C:\Program Files (x86)\Steam\steamapps\common\skyrim\Data
 SET TARGETDIR=%USERPROFILE%\Dropbox\SkyrimMod\PersistentLegacy\dist\Data
+SET DEPSOURCEDIR=D:\Projects\SKSE\DBM_Utils
+SET DEPTARGETDIR=%USERPROFILE%\Dropbox\SkyrimMod\PersistentLegacy\dist\dep\DBM_Utils
 
 xcopy /E /U /Y "%SOURCEDIR%\*" "%TARGETDIR%\"
+xcopy /E /D /U /Y "%DEPSOURCEDIR%\*" "%DEPTARGETDIR%\"
